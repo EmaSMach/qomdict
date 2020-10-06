80cdbb3752e8524b420fdcaed1defe2dbc65682d
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-10-06 04:23:18 -0300

    probando subida

--- qdict/settings.py
+++ qdict/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jn&cfn!088^=tm4vwxhpqz*sc3%#+_7$om9-*ohh$5y&6xkp$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
