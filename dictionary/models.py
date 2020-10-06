2fb490dd8e8b14146f555cc5c2bea27007ceb1a9
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-10-05 23:45:53 -0300

    created model for Word and ran migrations

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    qom = models.CharField(max_length=100, null=True, blank=True)
+    dfs = models.TextField(null=True, blank=True)
+    syn = models.CharField(max_length=300, null=True, blank=True)
+    var = models.CharField(max_length=300, null=True, blank=True)
+    see = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.qom or ""
