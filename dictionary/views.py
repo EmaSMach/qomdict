65351eb10b99498136500e17ab9c4543b71e9713
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-10-19 01:30:19 -0300

    added search functionality

--- dictionary/views.py
+++ dictionary/views.py
@@ -10,10 +10,30 @@ class HomeView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
-        context['words'] = Word.objects.all()
+        word = self.request.GET.get('word')
+        if word is None:
+            buscando = False
+        else:
+            word = word.strip().lower()
+            buscando = True
+        # context['words'] = Word.objects.all()
+        if word:
+            word = Word.objects.filter(qom=word).first()
+            context['word'] = word
+        context['buscando'] = buscando
         return context
         
 
 class WordListView(ListView):
     model = Word
     template_name = "dicionary/word_list.html"
+
+
+class WordDetailView(DetailView):
+    model = Word
+
+
+def search_word(request, word):
+    word = Word.objects.get(qom=word)
+    context = {'word': word or None}
+    return render(request, template_name='dicionary/index.html', context=context)
